769784d1dd153ad35ba46d45f209ed7160502bc1
jampool <jampoolgarcia@gmail.com>
2021-06-27 23:06:45 -0400

    Added form validation and user feedback.

--- src/app/contact-book/pages/contact-form/contact-form.component.ts
+++ src/app/contact-book/pages/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ContactI } from '../../interface/contact';
 
 @Component({
@@ -29,6 +30,19 @@ export class ContactFormComponent implements OnInit {
 
   onSubmit(){
     console.log(this.contact);
+  }
+
+  getErrorMsg(control: FormControl): string{
+
+    if(control.errors?.required){
+      return 'The field is required.'
+    }else if(control.errors?.minlength){
+      return `The field requires minimum ${control.errors?.minlength.requiredLength} characters.`;
+    }else if(control.errors?.pattern){
+      return 'The field is not in the correct format.'
+    }
+
+    return '';
   }
 
 }
